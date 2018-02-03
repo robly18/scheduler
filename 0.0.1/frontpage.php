f6f3f556a7081f89d1cb707e9702c1babf86e146
Winiandohr <31969778+Winiandohr@users.noreply.github.com>
2018-02-03 17:02:51 +0000

    Scheduler_Block

---
+++ 0.0.1/frontpage.php
@@ -0,0 +1,35 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="CSS/frontpage.css">
+		<title>Scheduler</title>
+		<meta charset="UTF-8"/>
+		<meta name="author" content="Jorge Alves", "Duarte Luís"/>
+		<meta name="description" content="Scheduler"/>
+	</head>
+	
+	<header>
+		<h1 id="thehead">Scheduler Alpha Prototype 0.0.1</h2>
+	</header>
+	
+	<body>
+		<article id="what_we_are">
+			<h2 id="titler">What we are</h3>
+			<p>We are a group of two people who have designed a scheduler for both individuals and groups with easy accessibility.</p>
+			<p>We thrive to achieve an easy option for our users, giving a fast and easy to understand interface</p>
+		</article>
+		
+		<article>
+			<h2 id="titler">What we do</h3>
+			<p>We offer you, the user, means to making your daily life easier, by introducing to you -Scheduler-(to be changed) where you can input
+			   events on a private profile, or a group one.</p>
+		</article>
+		
+		<form>
+			<input type="button" value="Let's get started." />
+		</form>
+	</body>
+	
+	<footer>
+		<p>This is a footer</p>
+	</footer>
+<html>
