aed6793248e58a7cc014e91035875838aaae3aae
robly18 <duarteluis.nascimento@hotmail.com>
2018-06-29 21:34:25 +0200

    added distinction block/displayElement; got thing working

--- 0.0.3/js/js.js
+++ 0.0.3/js/js.js
@@ -2,9 +2,7 @@
 
 function preprocess(list) { //Returns preprocessed list ready to be sent to fillColumn
 	let newlist = list.slice(0).sort((a,b) => a.startpct-b.startpct);
-	
 	for (let i = 0; i < newlist.length-1; i++) {
-		console.log(newlist);
 		if (newlist[i].endpct > newlist[i+1].startpct) {
 			/*If this happens:
 			[AAAAAAAA]
@@ -23,7 +21,7 @@ function preprocess(list) { //Returns preprocessed list ready to be sent to fill
 				first.endpct = newlist[i+1].startpct;
 				let overlap = Object.assign({}, newlist[i+1]);
 				overlap.endpct = newlist[i].endpct;
-				overlap.text = newlist[i].text.concat([" + "]).concat(newlist[i+1].text);
+				overlap.linkedblocks = newlist[i].linkedblocks.concat(newlist[i+1].linkedblocks);
 				overlap.color = "white";
 				let last = Object.assign({}, newlist[i+1]);
 				last.startpct = newlist[i].endpct;
@@ -42,7 +40,7 @@ function preprocess(list) { //Returns preprocessed list ready to be sent to fill
 				let first = Object.assign({}, newlist[i]);
 				first.endpct = newlist[i+1].startpct;
 				let overlap = Object.assign({}, newlist[i+1]);
-				overlap.text = newlist[i].text.concat([" + "]).concat(newlist[i+1].text);
+				overlap.linkedblocks = newlist[i].linkedblocks.concat(newlist[i+1].linkedblocks);
 				overlap.color = "white";
 				let last = Object.assign({}, newlist[i]);
 				last.startpct = newlist[i+1].endpct;
@@ -51,11 +49,11 @@ function preprocess(list) { //Returns preprocessed list ready to be sent to fill
 			}
 		}
 	}
-	return newlist.filter(block => block.startpct != block.endpct);
+	return newlist.filter(b => b.startpct != b.endpct);
 }
 
 
-function fillColumn(list, day) { /* list of {color, startpct, endpct, text} ordered with no overlap; color is optional and defaults to white */
+function fillColumn(list, day) { /* list of {color, startpct, endpct, linkedblocks} ordered with no overlap; color is optional and defaults to white */
 	var lastpct = 0; //where we left off
 	for (var block of list) {
 		if (block.startpct != lastpct) { //if we need padding, add it
@@ -66,64 +64,69 @@ function fillColumn(list, day) { /* list of {color, startpct, endpct, text} orde
 		}
 		
 		var b = document.createElement("div"); //the actual block element
-		//b.setAttribute("id", "div"+i);
-		//var title = document.getElementById("Title").value;
-		//var initialHour = document.getElementById("initialHour").value; //Javascript doesn't let me straight up attribute it so I have to do this stupid verbose thing.
-		//var initialMinute = document.getElementById("initialMinute").value;
-		//var endHour = document.getElementById("endHour").value;
-		//var endMinute = document.getElementById("endMinute").value;
-		//var description = document.getElementById("description").value;		
-		//b.setAttribute("title", "test"+title);
-		//b.setAttribute("initialHour", initialHour);
-		//b.setAttribute("initialMinute", initialMinute);
-		//b.setAttribute("endHour", endHour);
-		//b.setAttribute("endMinute", endMinute);
-		//b.setAttribute("description", description);
 		
 		
 		b.className = "clickableDiv";
 		b.style.backgroundColor = block.color || "white";
 		b.style.height = (block.endpct - block.startpct)+"%";
 		
-		b.onclick = function()
-		{
-			//var myDiv1 = document.getElementById(this.id);
-			var myDiv2 = document.getElementById("demonstrator");
-			//myDiv2.innerHTML = myDiv1.innerHTML;
-			
-			//myDiv2.innerHTML = "Title: "+ b.getAttribute("title") + "<br>" + "Duration: " + b.getAttribute("initialHour") + ':' + b.getAttribute("initialMinute")+ ' - ' + b.getAttribute("endHour") + ':' + b.getAttribute("endMinute") + "<br>" + "Description: " + b.getAttribute("description");
-			//myDiv2.innerHTML = "Title: "+ this.title + "ID: " + this.id;
-			//text:[initialHour+':'+initialMinute+' - '+ endHour+':'+endMinute, "Works?"]
-		}
+		let blocklist = block.linkedblocks;
+		b.onclick = () => displayBlocks(blocklist);
 		
-		for (var line of block.text) {
-			b.appendChild(document.createTextNode(line));
-			b.appendChild(document.createElement("BR"));
-		}
+		var titles = block.linkedblocks.map(bl => bl.title);
+		b.appendChild(document.createTextNode(titles.join(", ")));
+		
 		day.appendChild(b);
 		
 		lastpct = block.endpct;
-		i = i+1;
 	}
 }
 
-var blocks = [];
-var i = 1;
+var blocks = {};
+var currentId = 0;
+
+var displayElements = [];
 
 document.getElementById("addButton").onclick = function()
 {
 	var title = document.getElementById("Title").value;
+	var desc = document.getElementById("description").value;
 	var initialHour = document.getElementById("initialHour").value;
 	var initialMinute = document.getElementById("initialMinute").value;
 	var endHour = document.getElementById("endHour").value;
 	var endMinute = document.getElementById("endMinute").value;
-	//blocks.push({startpct: initialHour*hpct + initialMinute*mpct, endpct: endHour*hpct + endMinute*mpct, text:[initialHour+':'+initialMinute+' - '+ endHour+':'+endMinute, "Works?"]});
-	blocks.push({id: title, startpct: initialHour*hpct + initialMinute*mpct, endpct: endHour*hpct + endMinute*mpct, text: [title]});
+	blocks[currentId++] = { id: currentId,
+							year: 2018, month: 1, day: 1, startHour: initialHour, startMinute: initialMinute,
+															endHour: endHour,		endMinute: endMinute,
+							title: title, desc: desc, tags:[]};
+	refreshBlocks();
+}
+
+function refreshBlocks() { //Takes the Object "blocks" and puts it on the screen.
+	//begin by updating the array of display elements
+	displayElements = preprocess(
+						Object.values(blocks).map(
+										b => ({startpct: b.startHour*hpct + b.startMinute*mpct,
+										endpct: b.endHour*hpct + b.endMinute*mpct,
+										linkedblocks: [b]})
+										)
+					);
+	//clear out the "day"
 	let day = document.getElementById("day1");
 	day.innerHTML = "";
-	blocks = preprocess(blocks);
-	fillColumn(blocks, day);
-	i = i+1;
+	//and repopulate it
+	fillColumn(displayElements, day);
+}
+
+function displayBlocks(blocklist) {
+	var demonstrator = document.getElementById("demonstrator");
+	if (blocklist.length == 0) {
+		demonstrator.innerHTML = "N/A";
+	} else if (blocklist.length == 1) {
+		demonstrator.innerHTML = "one dude, titled " + blocklist[0].title;
+	} else {
+		demonstrator.innerHTML = "more dudes";
+	}
 }
 
 var hourStart = 0;
