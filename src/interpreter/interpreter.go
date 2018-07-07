a2e67883a219f4304fe1b13072e1c4229e16273c
robly18 <duarteluis.nascimento@hotmail.com>
2018-07-07 15:50:36 +0100

    support color and tags arguments now

--- src/interpreter/interpreter.go
+++ src/interpreter/interpreter.go
@@ -6,6 +6,7 @@ import (
 	"shlex"
 	"strconv"
 	"time"
+	"fmt"
 )
 
 func notImplementedError() error {
@@ -14,6 +15,10 @@ func notImplementedError() error {
 
 func wrongArgNo() error {
 	return errors.New("wrong argument number")
+}
+
+func syntaxError(i int) error {
+	return errors.New(fmt.Sprintf("wrong syntax on argument %v", i))
 }
 
 
@@ -34,9 +39,27 @@ func runArgs(args []string) (string, error) {
 		return "", nil
 	}
 	switch args[0] {
-	case "add": //add YEAR MONTH DAY STARTH STARTM ENDH ENDM TITLE DESC [-t TAG1 TAG2 ...] (todo tags and colors)
-		if len(args) != 10 {
+	case "add": //add YEAR MONTH DAY STARTH STARTM ENDH ENDM TITLE DESC [-c COLOR] [-t TAG1 TAG2 ...]
+		if len(args) < 10 {
 			return "", wrongArgNo()
+		}
+		color := 0x0000FF //default color: blue
+		tags := []string{}
+		for i := 10; i < len(args); i++ {
+			if args[i] == "-c" {
+				i++
+				c64, err := strconv.ParseInt(args[i], 16, 0)
+				if err != nil {
+					return "", err
+				}
+				color = int(c64)
+			} else if args[i] == "-t" {
+				for i++; i < len(args); i++ {
+					tags = append(tags, args[i])
+				}
+			} else {
+				return "", syntaxError(i)
+			}
 		}
 		var aints [7]int
 		for i := 1; i != 8; i++ {
@@ -48,7 +71,7 @@ func runArgs(args []string) (string, error) {
 		}
 		startDate := time.Date(aints[0], time.Month(aints[1]), aints[2], aints[3], aints[4], 0, 0, time.UTC) //For now, time is UTC
 		endDate := time.Date(aints[0], time.Month(aints[1]), aints[2], aints[5], aints[6], 0, 0, time.UTC)
-		b, err := core.MakeBlock(startDate, endDate, 0x0000FF, args[8], args[9], []string{})
+		b, err := core.MakeBlock(startDate, endDate, color, args[8], args[9], tags)
 		if err != nil {
 			return "", err
 		}
